a3a3945ae9daaf90d69f86e342677eb392747a57
SamiulIslam017 <samiul641@gmail.com>
2023-03-03 01:41:28 +0600

    feating completed from api

--- js/index.js
+++ js/index.js
@@ -1,10 +1,35 @@
 const loadAllData = () => {
     fetch("https://openapi.programming-hero.com/api/ai/tools")
     .then(res => res.json())
-    .then(data => showAllData(data));
+    .then(data => showAllData(data.data));
 
 }
 const showAllData = (data) => {
-    console.log(data);
+    data.tools.slice(0,6).forEach(data => {
+        const cards = document.getElementById('cards');
+        const div = document.createElement("div");
+        div.classList.add("card");
+        div.innerHTML = `
+        <img src="${data.image}">
+        <h2>Features</h2>
+        <ol>
+            <li>a</li>
+            <li>b</li>
+            <li>c</li>
+        </ol>
+        <hr>
+        <div class="card-footer">
+            <div class="left-section">
+                <h2>h</h2>
+                <p>j</p>
+            </div>
+            <div class="right-side">
+                <button>Details</button>
+            </div>
+        </div>
+        `
+        cards.appendChild(div);
+        console.log(data);
+    });
 }
 loadAllData();
