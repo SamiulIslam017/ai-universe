14219ad5823c52e4dfffc37c3d60e1a252d35d46
SamiulIslam017 <samiul641@gmail.com>
2023-03-03 15:57:18 +0600

    show more button work done

--- js/index.js
+++ js/index.js
@@ -1,35 +1,55 @@
 const loadAllData = () => {
     fetch("https://openapi.programming-hero.com/api/ai/tools")
     .then(res => res.json())
-    .then(data => showAllData(data.data));
+    .then(data => showAllData(data.data.tools.slice(0,6)));
+    
 
 }
 const showAllData = (data) => {
-    data.tools.slice(0,6).forEach(data => {
-        const cards = document.getElementById('cards');
-        const div = document.createElement("div");
-        div.classList.add("card");
-        div.innerHTML = `
-        <img src="${data.image}">
-        <h2>Features</h2>
-        <ol>
-            <li>${data.features[0]}</li>
-            <li>${data.features[1]}</li>
-            <li>${data.features[2]}</li>
-        </ol>
-        <hr>
+    // console.log(data.slice(0,6));
+    const container = document.getElementById('cards');
+    container.innerHTML="";
+    data.forEach(data => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML=`
+        <img src="${data.image}" alt="">
+            <h2 class= "fs-3 fw-bold">Features</h2>
+            <ol>
+                <li>${data.features[0]}</li>
+                <li>${data.features[1]}</li>
+                <li>${data.features[2]}</li>
+            </ol>
+        
         <div class="card-footer">
             <div class="left-section">
-                <h2>${data.name}</h2>
-                <p>Published-date : ${data.published_in}</p>
+                <h2 class= "fs-3 fw-bold">${data.name}</h2>
+                <p>Published Data: ${data.published_in}</p>
             </div>
             <div class="right-side">
                 <button>Details</button>
             </div>
         </div>
+
+    
+
         `
-        cards.appendChild(div);
-        console.log(data);
+        container.appendChild(div);
     });
 }
+
+
+const showAllDataTogether = () => {
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then(res => res.json())
+    .then(data =>{ 
+        showAllData(data.data.tools);
+        document.getElementById('show-more').classList.add(d-none);
+    });
+    
+}
+// const allData =(data) => {
+//     console.log(data);
+// }
+
 loadAllData();
